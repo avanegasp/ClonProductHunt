98bc1c41576493198a2c598c9c7cc21ef7c7f935
avanegasp <arco527@gmail.com>
2017-12-22 12:08:26 -0500

    Crea la ruta /products/new

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,11 @@
 class ProductsController < ApplicationController
 
   def index
-    @Products = Product.all
+    @products = Product.all
+  end
+
+#crear un nuevo producto, @product, es una nueva instacia de producto
+  def new
+    @product = Product.new
   end
 end
