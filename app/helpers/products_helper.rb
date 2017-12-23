f3b6674876b0994d5d197a389affe79c5062d53d
avanegasp <arco527@gmail.com>
2017-12-23 09:55:29 -0500

    Add link to edit, product

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,5 @@
 module ProductsHelper
   def form_title
-    @product.new_record? ? "Publicar Producto" : "Modificar Producto"
+    @product.new_record? ? "Nuevo Producto" : "Modificar Producto"
   end
 end
