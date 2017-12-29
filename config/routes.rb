20f8da19c8cd8ccaabb09dd80d312baabde6dab3
avanegasp <arco527@gmail.com>
2017-12-28 20:31:38 -0500

    Add comments to products

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,14 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
+
   delete 'logout', to: 'sessions#destroy'
   resources :users, only: [:new, :create]
-  resources :products
+  resources :products do
+    resources :comments, only: [:create]
+  end
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
